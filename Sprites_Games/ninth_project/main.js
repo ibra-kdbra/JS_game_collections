232f6c0bd30645c74e8d2e447ffe38c3edd9d57c
ibra-kdbra <git-repo-develop@outlook.com>
2024-03-21 16:28:11 +0300

    added the particles animation and UI debug, and deleted the spilce method, an added more efficient method

--- Sprites_Games/ninth_project/main.js
+++ Sprites_Games/ninth_project/main.js
@@ -2,36 +2,64 @@ import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
 import { ClimbingEnemy, FlyingEnemy, GroundEnemy } from "./enemies.js";
+import { UI } from "./UI.js";
 
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1')
     const ctx = canvas.getContext('2d');
-    canvas.width = 500;
+    canvas.width = 900;
     canvas.height = 500;
 
     class Game{
         constructor(width, height){
             this.width = width;
             this.height = height;
-            this.groundMargin = 80;
+            this.groundMargin = 40;
             this.speed = 0;
             this.maxSpeed = 4;
             this.background = new Background(this);
             this.player = new Player(this);
-            this.input = new InputHandler();
+            this.input = new InputHandler(this);
+            this.UI = new UI(this);
             this.enemies = [];
+            this.particles = [];
+            this.collisions = [];
+            this.floatingMessages = [];
+            this.maxParticles = 50;
             this.enemyTimer = 0;
             this.enemyInterval = 1000;
+            this.debug = false;
+            this.score = 0;
+            this.winningScore = 40;
+            this.fontColor = 'black';
+            this.time = 0;
+            this.maxTime = 30000;
+            this.gameOver = false;
+            this.lives = 5;
+            this.player.currentState = this.player.states[0];
+            this.player.currentState.enter();    
         }
         draw(context){
             this.background.draw(context);
             this.player.draw(context);
             this.enemies.forEach(enemy =>{
                 enemy.draw(context);
+            });
+            this.particles.forEach(particle =>{
+                particle.draw(context);
+            });
+            this.collisions.forEach(collision =>{
+                collision.draw(context);
             })
+            this.floatingMessages.forEach(message =>{
+                message.draw(context);
+            })
+            this.UI.draw(context);
         }
         update(deltaTime){
+            this.time += deltaTime;
+            if(this.time > this.maxTime) this.gameOver = true;
             this.background.update();
             this.player.update(this.input.keys, deltaTime);
             //handle Enemies
@@ -43,14 +71,31 @@ window.addEventListener('load', function(){
             }
             this.enemies.forEach(enemy =>{
                 enemy.update(deltaTime);
-                if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1)
+            });
+            //handle messages
+            this.floatingMessages.forEach(message =>{
+                message.update();
             })
+            //handle particles
+            this.particles.forEach((particle, index) =>{
+                particle.update();
+            });
+            if(this.particles.length > this.maxParticles){
+                this.particles.length = this.maxParticles;
+            }
+            //handle collision sprites
+            this.collisions.forEach((collision, index) =>{
+                collision.update(deltaTime);
+            })
+            this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
+            this.particles = this.particles.filter(particle => !particle.markedForDeletion);
+            this.collisions = this.collisions.filter(collision => !collision.markedForDeletion);
+            this.floatingMessages = this.floatingMessages.filter(message => !message.markedForDeletion);
         }
         addEnemy(){
             if(this.speed > 0 && Math.random() * 0.5) this.enemies.push(new GroundEnemy(this));
             if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
-            this.enemies.push(new FlyingEnemy(this))
-            console.log(this.enemies);
+            this.enemies.push(new FlyingEnemy(this));
         }
     }
     const game = new Game(canvas.width, canvas.height);
@@ -63,7 +108,7 @@ window.addEventListener('load', function(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         game.update(deltaTime);
         game.draw(ctx);
-        requestAnimationFrame(animate);
+        if(!game.gameOver) requestAnimationFrame(animate);
     }
     animate(0);
 })
