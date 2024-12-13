d2a9b765cbb4c17cae8171a4d1a30024c1ffde81
ibra-kdbra <git-repo-develop@outlook.com>
2024-12-13 23:55:45 +0300

    data collection

---
+++ assets/js/data.js
@@ -0,0 +1,74 @@
+export const games = [
+  {
+    link: "./Simple/index.html",
+    imgSrc: "./Simple/captured.png",
+    title: "Flow Fields (Simple)",
+    description: "Experiment with flow fields in this creative game. Press D to debug."
+  },
+  {
+    link: "./Text_Fields/index.html",
+    imgSrc: "./Text_Fields/captured.png",
+    title: "Text Fields",
+    description: "Experiment with flow fields in this creative game. Press D to debug."
+  },
+  {
+    link: "./Experimental_math/index.html",
+    imgSrc: "./Experimental_math/captured.png",
+    title: "Experimental Math",
+    description: "Low-level math with flow fields."
+  },
+  {
+    link: "./Adaptive/index.html",
+    imgSrc: "./Adaptive/images/result3.png",
+    title: "Adaptive Fields",
+    description: "Try multiple variations with adaptive flow fields."
+  },
+  {
+    link: "./2048/index.html",
+    imgSrc: "./2048/meta/apple-touch-icon.png",
+    title: "2048 with AI",
+    description: "Challenge yourself with the classic tile-merging puzzle. It has a built-in AI player, with some fantastic algorithms."
+  },
+  {
+    link: "./Sprites_Games/ninth_project/index.html",
+    imgSrc: "./Sprites_Games/ninth_project/images/captured.png",
+    title: "Sprite Game",
+    description: "Explore adventure with this sprite-based game."
+  },
+  {
+    link: "./Hangman/index.html",
+    imgSrc: "./Hangman/images/captured.png",
+    title: "Hangman",
+    description: "Classic Hangman game with multiple options, wonderful game play."
+  },
+  {
+    link: "./Flow_fields/index.html",
+    imgSrc: "./assets/icons/captured.png",
+    title: "Flow Fields",
+    description: "Math with Art create wonders"
+  },
+  {
+    link: "./PacMan_Slasher/index.html",
+    imgSrc: "./PacMan_Slasher/img/screen.png",
+    title: "PacMan Slasher",
+    description: "Defeat a pacman by drawing its symbol anywhere."
+  },
+  {
+    link: "./Tron/index.html",
+    imgSrc: "./Tron/imgs/intro.png",
+    title: "Tron",
+    description: "Remake of the classic arcade game that involves maneuvering a 'light cycle' in an arena against an opponent."
+  },
+  {
+    link: "./Tetris/index.html",
+    imgSrc: "./Tetris/imgs/captured.png",
+    title: "Tetris",
+    description: "The original Tetris game, with a few enhancements, many options included."
+  },
+  {
+    link: "./MoonLDR/index.html",
+    imgSrc: "./MoonLDR/images/normal.png",
+    title: "MoonLDR",
+    description: "Fun landing ship on ground game, it's a classic based on Lunar Lander"
+  },
+];
