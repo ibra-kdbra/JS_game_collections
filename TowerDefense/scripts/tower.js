ed7668613804d5778a7cb93a2a9c953757ba2fc2
ibra-kdbra <git-repo-develop@outlook.com>
2025-02-19 23:01:14 +0300

    tower class

---
+++ TowerDefense/scripts/tower.js
@@ -0,0 +1,164 @@
+class Tower {
+    constructor(col, row) {
+        // Display
+        this.baseOnTop = true;      // render base over barrel
+        this.border = [0, 0, 0];    // border color
+        this.color = [0, 0, 0];     // main color
+        this.drawLine = true;       // draw line to enemy on attack
+        this.follow = true;         // follow target even when not firing
+        this.hasBarrel = true;
+        this.hasBase = true;
+        this.length = 0.7;          // barrel length in tiles
+        this.radius = 1;            // radius in tiles
+        this.secondary = [0, 0, 0]; // secondary color
+        this.weight = 2;            // laser stroke weight
+        this.width = 0.3;           // barrel width in tiles
+
+        // Misc
+        this.alive = true;
+        this.name = 'tower';
+        this.sound = null;          // sound to play on fire
+        this.title = 'Tower';
+
+        // Position
+        this.angle = 0;
+        this.gridPos = createVector(col, row);
+        this.pos = createVector(col * ts + ts / 2, row * ts + ts / 2);
+        
+        // Stats
+        this.cooldownMax = 0;
+        this.cooldownMin = 0;
+        this.cost = 0;
+        this.damageMax = 20;
+        this.damageMin = 1;
+        this.range = 3;
+        this.totalCost = 0;
+        this.type = 'physical';     // damage type
+        this.upgrades = [];
+    }
+
+    // Adjust angle to point towards pixel position
+    aim(x, y) {
+        this.angle = Math.atan2(y - this.pos.y, x - this.pos.x);
+    }
+
+    // Deal damage to enemy
+    attack(e) {
+        const damage = Math.round(Math.random() * (this.damageMax - this.damageMin) + this.damageMin);
+        e.dealDamage(damage, this.type);
+        if (!muteSounds && sounds.hasOwnProperty(this.sound)) {
+            sounds[this.sound].play();
+        }
+        this.onHit(e);
+    }
+
+    // Check if cooldown is completed
+    canFire() {
+        return this.cd === 0;
+    }
+
+    draw() {
+        // Draw turret base
+        if (this.hasBase && !this.baseOnTop) this.drawBase();
+        // Draw barrel
+        if (this.hasBarrel) {
+            push();
+            translate(this.pos.x, this.pos.y);
+            rotate(this.angle);
+            this.drawBarrel();
+            pop();
+        }
+        // Draw turret base
+        if (this.hasBase && this.baseOnTop) this.drawBase();
+    }
+
+    // Draw barrel of tower (moveable part)
+    drawBarrel() {
+        stroke(this.border);
+        fill(this.secondary);
+        rect(0, -this.width * ts / 2, this.length * ts, this.width * ts);
+    }
+
+    // Draw base of tower (stationary part)
+    drawBase() {
+        stroke(this.border);
+        fill(this.color);
+        ellipse(this.pos.x, this.pos.y, this.radius * ts, this.radius * ts);
+    }
+
+    // Returns damage range
+    getDamage() {
+        return rangeText(this.damageMin, this.damageMax);
+    }
+
+    // Returns average cooldown in seconds
+    getCooldown() {
+        return (this.cooldownMin + this.cooldownMax) / 120;
+    }
+
+    kill() {
+        this.alive = false;
+    }
+
+    isDead() {
+        return !this.alive;
+    }
+
+    // Functionality once entity has been targeted
+    onAim(e) {
+        if (this.canFire() || this.follow) this.aim(e.pos.x, e.pos.y);
+        if (stopFiring) return;
+        if (!this.canFire()) return;
+        this.resetCooldown();
+        this.attack(e);
+        // Draw line to target
+        if (!this.drawLine) return;
+        stroke(this.color);
+        strokeWeight(this.weight);
+        line(this.pos.x, this.pos.y, e.pos.x, e.pos.y);
+        strokeWeight(1);
+    }
+
+    onCreate() {
+        this.cd = 0;                // current cooldown left
+    }
+
+    onHit(e) {}
+
+    resetCooldown() {
+        this.cd = Math.round(Math.random() * (this.cooldownMax - this.cooldownMin) + this.cooldownMin);
+    }
+
+    // Sell price
+    sellPrice() {
+        return Math.floor(this.totalCost * sellConst);
+    }
+
+    // Target correct enemy
+    target(entities) {
+        entities = this.visible(entities);
+        if (entities.length === 0) return;
+        let t = getTaunting(entities);
+        if (t.length > 0) entities = t;
+        let e = getFirst(entities);
+        if (typeof e === 'undefined') return;
+        this.onAim(e);
+    }
+
+    update() {
+        if (this.cd > 0) this.cd--;
+    }
+
+    // Use template to set attributes
+    upgrade(template = {}) {
+        Object.keys(template).forEach(key => {
+            this[key] = template[key];
+        });
+        if (template.cost) this.totalCost += template.cost;
+    }
+
+    // Returns array of visible entities out of passed array
+    visible(entities) {
+        return getInRange(this.pos.x, this.pos.y, this.range, entities);
+    }
+}
