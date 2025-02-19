97a6e694752b63ae6492b6ba63a0c9c81bfb0ae6
ibra-kdbra <git-repo-develop@outlook.com>
2025-02-19 23:31:02 +0300

    removed comments

--- PacMan/pacman.js
+++ PacMan/pacman.js
@@ -10,25 +10,6 @@
 
 (function(){
 
-//@line 1 "src/inherit.js"
-//  Apparently, the mutable, non-standard __proto__ property creates a lot of complexity for JS optimizers,
-//   so it may be phased out in future JS versions.  It's not even supported in Internet Explorer.
-//
-//  Object.create does everything that I would use a mutable __proto__ for, but this isn't implemented everywhere yet.
-// 
-//  So instead of the following:
-//
-//      var obj = {
-//          __proto__: parentObj,
-//          hello: function() { return "world"; },
-//      };
-//
-//  You can use this:
-//
-//      var obj = newChildObject(parentObj, {
-//          hello: function() { return "world"; },
-//      };
-
 var newChildObject = function(parentObj, newObj) {
 
     // equivalent to: var resultObj = { __proto__: parentObj };
