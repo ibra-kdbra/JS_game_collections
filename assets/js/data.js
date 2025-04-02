38f8b943698bb700c24951d06f3f1cd53a1c9c30
ibra-kdbra <git-repo-develop@outlook.com>
2025-04-02 23:55:02 +0300

    updated for the tags

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,84 +3,99 @@ export const games = [
     link: "./Simple/index.html",
     imgSrc: "./Simple/captured.png",
     title: "Flow Fields (Simple)",
-    description: "Experiment with flow fields in this creative game. Press D to debug."
+    description: "Experiment with flow fields. Press D to debug or double touch.",
+    "tags": ["#PC", "#Mobile"]
   },
   {
     link: "./Text_Fields/index.html",
     imgSrc: "./Text_Fields/captured.png",
     title: "Text Fields",
-    description: "Experiment with flow fields in this creative game. Press D to debug."
+    description: "Experiment with flow fields. Press D to debug or double touch.",
+    "tags": ["#PC", "#Mobile"]
+
   },
   {
     link: "./Experimental_math/index.html",
     imgSrc: "./Experimental_math/captured.png",
     title: "Experimental Math",
-    description: "Low-level math with flow fields."
+    description: "Low-level math with flow fields.",
+    tags: ["#PC", "#Mobile"]
   },
   {
     link: "./Adaptive/index.html",
     imgSrc: "./Adaptive/images/result3.png",
     title: "Adaptive Fields",
-    description: "Try multiple variations with adaptive flow fields."
+    description: "Try multiple variations with adaptive flow fields.",
+    tags: ["#PC", "#Mobile"]
   },
   {
     link: "./2048/index.html",
     imgSrc: "./2048/meta/apple-touch-icon.png",
     title: "2048 with AI",
-    description: "Challenge yourself with the classic tile-merging puzzle. It has a built-in AI player, with some fantastic algorithms."
+    description: "Challenge yourself with the classic tile-merging puzzle. It has a built-in AI player, with some fantastic algorithms.",
+    tags: ["#PC", "#Mobile"]
   },
   {
     link: "./TowerDefense/index.html",
     imgSrc: "./TowerDefense/imgs/captured.png",
     title: "Tower Defense Game",
-    description: "A tower defense game with many options built with p5.js, with a few enhancements, many modes, playgrounds provided."
+    description: "A tower defense game with many options built with p5.js, with a few enhancements, many modes, playgrounds provided.",
+    tags: ["#PC"]
   },
   {
     link: "./Hangman/index.html",
     imgSrc: "./Hangman/images/captured.png",
     title: "Hangman",
-    description: "Classic Hangman game with multiple options, wonderful game play."
+    description: "Classic Hangman game with multiple options, wonderful game play.",
+    tags: ["#PC", "#Mobile"]
   },
   {
     link: "./Flow_fields/index.html",
     imgSrc: "./assets/icons/captured.png",
     title: "Flow Fields",
-    description: "Math with Art create wonders"
+    description: "Math with Art create wonders",
+    tags: ["#PC", "#Mobile"]
   },
   {
     link: "./PacMan_Slasher/index.html",
     imgSrc: "./PacMan_Slasher/img/screen.png",
     title: "PacMan Slasher",
-    description: "Defeat a pacman by drawing its symbol anywhere."
+    description: "Defeat a pacman by drawing its symbol anywhere.",
+    tags: ["#PC"]
   },
   {
     link: "./Tron/index.html",
     imgSrc: "./Tron/imgs/intro.png",
     title: "Tron",
-    description: "Remake of the classic arcade game that involves maneuvering a 'light cycle' in an arena against an opponent."
+    description: "Remake of the classic arcade game that involves maneuvering a 'light cycle' in an arena against an opponent.",
+    tags: ["#PC"]
   },
   {
     link: "./Tetris/index.html",
     imgSrc: "./Tetris/imgs/captured.png",
     title: "Tetris",
-    description: "The original Tetris game, with a few enhancements, many options included."
+    description: "The original Tetris game, with a few enhancements, many options included.",
+    tags: ["#PC"]
   },
   {
     link: "./PacMan/index.html",
     imgSrc: "./PacMan/imgs/color03.png",
     title: "PacMan",
-    description: "The original PacMan game, with a few enhancements, many modes, playgrounds provided."
+    description: "The original PacMan game, with a few enhancements, many modes, playgrounds provided.",
+    tags: ["#PC"]
   },
   {
     link: "./MoonLDR/index.html",
     imgSrc: "./MoonLDR/images/normal.png",
     title: "MoonLDR",
-    description: "Fun landing ship on ground game, it's a classic based on Lunar Lander"
+    description: "Fun landing ship on ground game, it's a classic based on Lunar Lander",
+    tags: ["#PC", "#Mobile"]
   },
   {
     link: "./Sprites_Games/ninth_project/index.html",
     imgSrc: "./Sprites_Games/ninth_project/images/captured.png",
     title: "Sprite Game",
-    description: "Explore adventure with this sprite-based game."
+    description: "Explore adventure with this sprite-based game.",
+    tags: ["#PC", "#Mobile"]
   },
 ];
