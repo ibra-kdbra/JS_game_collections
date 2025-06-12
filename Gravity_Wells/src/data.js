fbd1fa3b4c3f77e5512f4989d7a4131b26d2e04e
ibra-kdbra <git-repo-develop@outlook.com>
2025-06-12 23:57:18 +0300

    data from manifest

---
+++ Gravity_Wells/src/data.js
@@ -0,0 +1,34 @@
+let manifest;
+try {
+  const response = await fetch('../manifest.json');
+  manifest = await response.json();
+} catch (error) {
+  console.error('Error loading manifest.json:', error);
+  manifest = {}; // fallback empty object
+}
+let data = {};
+
+let lsData = localStorage.getItem(
+    manifest.name + ' Data'
+);
+
+if (lsData !== null) {
+    data = JSON.parse(lsData);
+}
+
+let save = (key, value) => {
+    data[key] = value;
+    localStorage.setItem(
+        manifest.name + ' Data',
+        JSON.stringify(data)
+    );
+};
+
+let load = (key) => {
+    return data[key];
+};
+
+export default Object.freeze({
+    save,
+    load
+});
